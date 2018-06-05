183e089c0f6cad5ad0dc0035b20eba7c884e2572
Ivo Huntjens <gamerkeeperson@gmail.com>
2018-06-05 19:53:47 +0200

    Add package-info.java to dtos pacakge

---
+++ src/main/java/com/rekeningrijden/europe/dtos/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * This package defines all data transfer objects which are needed for a uniform
+ * communication between all European countries and their systems.
+ */
+package com.rekeningrijden.europe.dtos;
